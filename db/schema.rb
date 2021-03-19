ab0aafc9a159c5a343a6a78b81b7741b710332e5
Randall Chaves Camacho <rchavesc1@gmail.com>
2021-03-18 23:12:33 -0600

    heroku error fix first try

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_19_025350) do
+ActiveRecord::Schema.define(version: 2021_03_18_184346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,12 +58,6 @@ ActiveRecord::Schema.define(version: 2021_03_19_025350) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["tweet_id"], name: "index_likes_on_tweet_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
-  end
-
-  create_table "tags", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "tweets", force: :cascade do |t|
