15226259365d426d7e5797ccc854675509df96f9
Randall Chaves Camacho <rchavesc1@gmail.com>
2021-03-18 22:36:31 -0600

    historia 1 y 4 de hito 2 needs work

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -16,8 +16,28 @@ class Tweet < ApplicationRecord
   # has_many :retweets, class_name: 'Tweet', foreign_key: 'retweet_id'
   # belongs_to :retweet, class_name: 'Tweet', optional: true
 
+  # attr_accessor :content, :name, :tag_list
 
+  # has_many :taggings
+  # has_many :tags, through: :taggings
 
+  # def self.tagged_with(name)
+  #   Tag.find_by_name!(name).tweets
+  # end
+
+  # def self.tag_counts 
+  #   Tag.select("tags.*, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id")
+  # end
+
+  # def tag_list
+  #   tags.map(&:name).join(", ")
+  # end
+
+  # def tag_lists=(names)
+  #   self.tags = names.split(",").map do |n|
+  #     Tag.where(name: n.strip).first_or_create!
+  #   end
+  # end
 
   
 end
